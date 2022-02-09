2561df4a37a2cfc83164f77268f55998cdc0515f
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-02-08 23:53:30 -0500

    front cover and back cover skeleton

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,10 +6,21 @@ import picture from "../images/profile.jpg";
 import picture2 from "../images/profile.jpg";
 import picture3 from "../images/profile.jpg";
 
+import HEART_IMAGE from "../images/Heart.png";
+import honeypot from "../images/honeypot.png";
+
 const Card = () => {
   return (
     <div className="card-container">
-      <div className="card cover-page-back ">
+      <div className="card cover-page-front">
+        <h2 className="card-title">To My Honey</h2>
+        <img className="honey-heart-img heart-1" src={HEART_IMAGE} alt="honey pot" />
+        <img className="honey-heart-img heart-2" src={HEART_IMAGE} alt="honey pot" />
+        <img className="honey-heart-img heart-3" src={HEART_IMAGE} alt="honey pot" />
+        <img className="honey-img" src={honeypot} alt="honey pot" />
+      </div>
+
+      {/* <div className="card cover-page-back ">
         <Polaroid pic={picture} left={"0%"} top={"0%"} rotate={-15} text="blahblahblah" />
         <Polaroid pic={picture2} left={"45%"} top={"25%"} rotate={15} text="blahblahblah" />
         <Polaroid pic={picture3} left={"3%"} top={"58%"} rotate={-5} text="blahblahblah" />
@@ -61,6 +72,12 @@ const Card = () => {
             et tortor at risus viverra.
           </p>
           <h2 className="message-signature">From Honey</h2>
+        </div>
+      </div> */}
+      <div className="card back-page-back">
+        <div className="closing-container">
+          <h2 className="closing-header">Love you always</h2>
+          <img className="closing-media" src="" alt="slideshow/video" />
         </div>
       </div>
     </div>
