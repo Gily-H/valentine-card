dd54bc7ae7eacc5ab17993c806526b3819d212cc
Gilman Huang <67421795+Gily-H@users.noreply.github.com>
2022-02-14 13:12:18 -0500

    full card animation without buttons

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 import FrontCover from "./FrontCover";
 import BackCover from "./BackCover";
 import "../../styles/card-styles/Card.css";
 
 const Card = () => {
+  const [frontOpen, setFrontOpen] = useState(false);
+  const [backOpen, setBackOpen] = useState(false);
+  const [isForward, setIsForward] = useState(false);
+
+  const changeCoverStatus = () => {
+    setFrontOpen((prevFrontOpen) => !prevFrontOpen);
+  };
+
+  const changeBackStatus = () => {
+    setBackOpen((prevBackOpen) => !prevBackOpen);
+  };
+
+  const bringForward = () => {
+    setIsForward(true);
+  };
+
   return (
-    <div className="card-container">
-      <FrontCover />
-      {/* <BackCover /> */}
-    </div>
+    <>
+      <div className="card-container">
+        <BackCover
+          openCloseHandler={changeBackStatus}
+          backStatus={backOpen}
+          isForward={isForward}
+          bringForward={bringForward}
+        />
+      </div>
+      <div className="card-container">
+        <FrontCover openCloseHandler={changeCoverStatus} coverStatus={frontOpen} />
+      </div>
+    </>
   );
 };
 
--- src/components/card/FrontCover.js
+++ src/components/card/FrontCover.js
@@ -1,22 +1,27 @@
-import React, { useState } from "react";
+import React from "react";
 import Polaroid from "../Polaroid";
 
 import picture from "../../images/profile.jpg";
 import picture2 from "../../images/profile.jpg";
 import picture3 from "../../images/profile.jpg";
 import HEART_IMAGE from "../../images/Heart.png";
-import honeypot from "../../images/honeypot.png";
+import HONEY_POT_IMAGE from "../../images/honeypot.png";
 import "../../styles/card-styles/FrontCover.css";
 
-const FrontCover = () => {
+const FrontCover = (props) => {
+  const changeStatus = () => {
+    props.openCloseHandler();
+    props.pushBack();
+  };
+
   return (
-    <div className="card cover-page">
-      <div className="cover-page-front">
+    <div className={`card cover-page ${props.coverStatus ? "flip-cover" : ""} `}>
+      <div className="cover-page-front" onClick={changeStatus}>
         <h2 className="card-title">To My Honey</h2>
         <img className="honey-heart-img heart-1" src={HEART_IMAGE} alt="honey pot" />
         <img className="honey-heart-img heart-2" src={HEART_IMAGE} alt="honey pot" />
         <img className="honey-heart-img heart-3" src={HEART_IMAGE} alt="honey pot" />
-        <img className="honey-img" src={honeypot} alt="honey pot" />
+        <img className="honey-img" src={HONEY_POT_IMAGE} alt="honey pot" />
       </div>
       <div className="cover-page-back">
         <Polaroid className="polaroid-1" pic={picture} text="blahblahblah" />
